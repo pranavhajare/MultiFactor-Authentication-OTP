eb20e3a74305cee1f0b8e0339249150395f4af06
pranavhajare <hajare.pranav@gmail.com>
2024-05-14 10:30:03 +0530

    create routes

---
+++ server/routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+const authControllers = require('../controllers/authControllers');
+
+router.post('/login', authControllers.login);
+
+router.post('/verify-otp', authControllers.verifyOtp);
+
+module.exports = router;
