49f1d4f0a8931220ab80c31666650d1c1a7bc665
pranavhajare <hajare.pranav@gmail.com>
2024-05-14 10:33:03 +0530

    add cors

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
 require('./config/db'); //Initialize the database connection
 
 const app = express();
+const PORT = 3001 || process.env.PORT;
 
-const PORT = 3001;
+
+app.use(bodyParser.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+const authRoutes = require('./routes/auth'); // Import authentication routes
+app.use('/auth', authRoutes); // Use authentication routes
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
